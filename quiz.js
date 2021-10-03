22bff026a578297f3ed05978ed97437c2bed3261
ped4735 <ped4735@gmail.com>
2021-10-03 19:41:06 -0300

    Utilizando bootstrap e jquery

--- quiz.js
+++ quiz.js
@@ -1,106 +1,138 @@
-//https://stackoverflow.com/questions/247483/http-get-request-in-javascript
-var HttpClient = function () {
-  this.get = function (aUrl, aCallback) {
-    var anHttpRequest = new XMLHttpRequest();
-    anHttpRequest.onreadystatechange = function () {
-      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-        aCallback(anHttpRequest.responseText);
-    };
-
-    anHttpRequest.open("GET", aUrl, true);
-    anHttpRequest.send(null);
-  };
-};
-
-//https://stackoverflow.com/questions/7394748/whats-the-right-way-to-decode-a-string-that-has-special-html-entities-in-it/7394787#7394787
-function decodeHtml(html) {
-  var txt = document.createElement("textarea");
-  txt.innerHTML = html;
-  return txt.value;
-}
-
-//https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+  //https://stackoverflow.com/questions/7394748/whats-the-right-way-to-decode-a-string-that-has-special-html-entities-in-it/7394787#7394787
+  function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
   }
-}
-
-const body = document.querySelector("body");
-const container = document.querySelector(".container");
-const display = document.querySelector(".display");
-const ol = document.querySelector("ol");
-const statusResp = document.querySelector(".status");
-
-display.style.display = "none";
-var resposta;
-
-//Consome API Trivia - HTTP GET
-//https://opentdb.com/api_config.php
-const client = new HttpClient();
-getQuestion();
-function getQuestion() { 
-  client.get("https://opentdb.com/api.php?amount=1&category=15&type=multiple", function (response) {
-    resposta = JSON.parse(response);
-    showQuestion(resposta);
-  });
-}
-
-function showQuestion(questions) {
-  display.style.display = "block";
-  ol.style.display = "block";
-  display.textContent = decodeHtml(questions.results[0].question);
-  // console.log(decodeHtml(questions.results[0].question))
-  //console.log(questions.results[0])
-  //(questions.results[0].correct_answer)
-  options = questions.results[0].incorrect_answers;
-  options.push(questions.results[0].correct_answer);
-  shuffleArray(options);
-  for (const question of options) {
-    //console.log(question)
-    let li = document.createElement("li");
-    li.appendChild(document.createTextNode(decodeHtml(question)));
-    li.onclick = () => {
-      handleChoise(li, questions.results[0].correct_answer);
-    };
-    ol.appendChild(li);
-  }
-
-  function handleChoise(li, correct_answer) {
-    if (li.textContent === correct_answer) {
-      corretResp();
-    } else {
-      incorretResp();
+  
+  //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
     }
-    console.log(li.innerHTML);
-
-    setTimeout(getQuestionAfterTime, 1500);
   }
 
-  function corretResp() {
-    console.log("correct!");
-    Array.from(body.getElementsByClassName("esconder")).forEach((element) => {
-      element.style.display = "none";
+window.onload = function() {
+    getQuestionFromAPI();
+    getCategoriesFromAPI();
+}
+
+var question
+let current_category = ""
+let current_difficulty = ""
+const question_display = document.querySelector('.question');
+const alternatives = document.querySelectorAll('.form-control[type="button"]')
+
+
+document.querySelector('.button-next').onclick = function () {
+    getQuestionFromAPI()    
+}
+
+
+/*document.querySelectorAll('select#my-select-dif').forEach(element => {
+    element.onchange = function() {
+      console.log(this.value)
+      current_difficulty = this.value
+      getQuestionFromAPI()
+    }
+  });*/
+
+
+$('select#my-select-dif').change(function () {
+    // console.log(this.value)
+    current_difficulty = this.value
+    getQuestionFromAPI()
+})
+
+
+/*document.querySelectorAll('select#my-select-cat').forEach(element => {
+    element.onchange = function() {
+      console.log(this.value)
+      current_category = this.value
+      getQuestionFromAPI()
+    }
+  });*/
+
+$('select#my-select-cat').change(function () {
+    // console.log(this.value)
+    current_category = this.value
+    getQuestionFromAPI()
+})
+
+$('.form-control[type="button"]').click(function () {
+  $('.button-next').attr('disabled','disabled')
+  $('.form-control[type="button"]').each((i,e) =>{
+    if(question.correct_answer == e.value){
+      $(e).addClass('success')
+    }else{
+      $(e).addClass('error')
+    }
+  })
+  setTimeout(getQuestionFromAPI,2000)
+})
+
+function resetDefaultBtns() {
+  $('.button-next').removeAttr('disabled')
+  $('.form-control[type="button"]').removeClass('success error')
+}
+
+
+function getQuestionFromAPI() {
+    $.ajax({
+        url: `https://opentdb.com/api.php?amount=1&category=${current_category}&difficulty=${current_difficulty}&type=multiple`,
+        context: document.body,
+        success: function(data){
+          //console.log(data)
+          resetDefaultBtns()
+          showQuestion(data)
+        }
     });
-    statusResp.innerHTML = "CORRETO";
-  }
+}
 
-  function incorretResp() {
-    console.log("errado!");
-    Array.from(body.getElementsByClassName("esconder")).forEach((element) => {
-      element.style.display = "none";
+function getCategoriesFromAPI(){
+    $.ajax({
+        url: "https://opentdb.com/api_category.php",
+        context: document.body,
+        success: function(data){
+          //console.log(data)
+          loadCategories(data)
+        }
     });
-    statusResp.innerHTML = "INCORRETO";
-  }
+}
 
-  function getQuestionAfterTime() {
-    statusResp.innerHTML = "";
+function loadCategories(data) {
+    
+    let select = document.querySelector('#my-select-cat')
+    categories = data.trivia_categories
 
-    while (ol.firstChild) {
-      ol.removeChild(ol.lastChild);
+    for (let i = 0; i < categories.length; i++) {
+        //console.log(`${categories[i].id} ${categories[i].name}`)
+        let opt = document.createElement('option')
+
+        let name = categories[i].name
+        if(name.indexOf(':')>0){
+            name = name.slice(name.indexOf(':')+1)
+        }
+        opt.value = categories[i].id
+        opt.innerHTML = name
+        select.appendChild(opt)
     }
 
-    getQuestion();
-  }
+}
+
+function showQuestion(data) {
+    question = data.results[0]
+    question_display.innerHTML = decodeHtml(data.results[0].question)
+
+    let answers = data.results[0].incorrect_answers
+    answers.push(data.results[0].correct_answer)
+    shuffleArray(answers)
+    console.log(question)
+
+
+    for (let i = 0; i < alternatives.length; i++) {
+        alternatives[i].value = decodeHtml(answers[i])
+    }
+
 }
